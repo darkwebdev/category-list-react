3bd874011c496f4ffa082fbdb80b7554c573c1ec
tibalt <gtibalt@gmail.com>
2015-10-19 00:31:50 +0200

    app component added

---
+++ src/App.js
@@ -0,0 +1,69 @@
+import React from 'react';
+
+import NewCat from '../src/NewCat';
+import Search from '../src/Search';
+import CatList from '../src/CatList';
+
+export default class App extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.catModel = props.catModel;
+
+        this.state = {
+            newCatActive: props.newCatActive,
+            searchActive: props.searchActive,
+            catList: this.catModel.collection
+        };
+    }
+
+    activateNewCat() {
+        this.setState({
+            newCatActive: true
+        });
+    }
+    disactivateNewCat() {
+        this.setState({
+            newCatActive: false
+        });
+    }
+    submitForm(cat) {
+        this.disactivateNewCat();
+        this.addCat(cat);
+    }
+    addCat(cat) {
+        this.catModel.add(cat);
+    }
+    removeCat(id) {
+        this.catModel.remove(id);
+    }
+
+    render() {
+        return (
+            <main>
+                <NewCat
+                    active={ this.state.newCatActive }
+                    onClick={ this.activateNewCat.bind(this) }
+                    onSubmit={ this.submitForm.bind(this) }
+                />
+                <Search
+                    active={ this.state.searchActive }
+                />
+                <CatList
+                    list={ this.state.catList }
+                    onRemove={ this.removeCat.bind(this) }
+                />
+            </main>
+        );
+    }
+}
+App.propTypes = {
+    newCatActive: React.PropTypes.bool,
+    searchActive: React.PropTypes.bool,
+    catModel: React.PropTypes.object.isRequired
+};
+
+App.defaultProps = {
+    newCatActive: false,
+    searchActive: false
+};
---
+++ test/app.js
@@ -0,0 +1,37 @@
+import _ from 'lodash';
+import { expect } from 'chai';
+
+import NewCat from '../src/NewCat';
+import Search from '../src/Search';
+import CatList from '../src/CatList';
+
+import fake from './fake';
+
+describe('App', () => {
+    'use strict';
+
+    let $app;
+
+    describe('by default', () => {
+        beforeEach(() => {
+            $app = fake.renderComp({ catModel: fake.catModel });
+        });
+
+        it('should show Add Category input in default state', () => {
+            expect($app.find(NewCat).length).to.equal(1);
+            expect($app.find(NewCat)[0].props.active).to.be.false;
+        });
+
+        it('should show Search input in default state', () => {
+            expect($app.find(Search).length).to.equal(1);
+            expect($app.find(Search)[0].props.active).to.be.false;
+        });
+
+        it('should show Category List', () => {
+            expect($app.find(CatList).length).to.equal(1);
+            expect($app.find(CatList)[0].props.list).to.deep.equal(fake.catList);
+        });
+    });
+
+});
+
