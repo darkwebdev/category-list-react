66b891a80aa0caff8984d9541b8064ed7ad03092
tibalt <gtibalt@gmail.com>
2015-10-19 00:27:42 +0200

    fakes extracted from category list tests

--- test/CatList.js
+++ test/CatList.js
@@ -8,49 +8,31 @@ chai.use(sinonChai);
 import CatList from '../src/CatList';
 import CatItem from '../src/CatItem';
 
+import fake from './fake';
+
 describe('Category list', () => {
     'use strict';
-
-    const fakeCatList = [
-        {
-            id: '1',
-            name: 'cat1',
-            number: '12'
-        },
-        {
-            id: '2',
-            name: 'cat 2',
-            number: '45'
-        },
-        {
-            id: '3',
-            name: 'cat number three',
-            number: '1'
-        }
-    ];
 
     const fakeRemoveHandler = sinon.spy();
 
     const createComp = () => {
         const catList = new CatList({
-            list: fakeCatList,
+            list: fake.catList,
             onRemove: fakeRemoveHandler
         });
         return $(catList.render()).shallowRender();
     };
 
     it('should show all Categories', () => {
-        expect(createComp().find(CatItem).length).to.equal(fakeCatList.length);
+        expect(createComp().find(CatItem).length).to.equal(fake.catList.length);
     });
 
     describe('on each child Remove Handler call', () => {
         it('should call parent Remove Handler', () => {
-            sinon.spy(dispatcher, 'dispatch');
-
             createComp().find(CatItem).each((catItem, i) => {
                 catItem.props.onRemove();
 
-                expect(fakeRemoveHandler).to.be.calledWith(fakeCatList[i].id);
+                expect(fakeRemoveHandler).to.be.calledWith(fake.catList[i].id);
             });
         });
     });
