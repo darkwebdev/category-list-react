eb67a8ed9f4a30091e2afa83c46c3a69b69686c6
tibalt <gtibalt@gmail.com>
2015-10-18 23:55:41 +0200

    category model add, remove methods added

--- src/CatModel.js
+++ src/CatModel.js
@@ -2,13 +2,20 @@ export default class {
     constructor(storage) {
         this.storage = storage;
         this.storageName = 'categories';
-        var savedCollection = storage.getItem(this.storageName);
+        const savedCollection = storage.getItem(this.storageName);
         this.savedCollection = savedCollection ? JSON.parse(savedCollection) : [];
     }
     get collection() {
         return this.savedCollection;
     }
     set collection(newCollection) {
+        this.savedCollection = newCollection;
         this.storage.setItem(this.storageName, JSON.stringify(newCollection));
     }
-};
+    remove(id) {
+        this.collection = this.collection.filter(model => model.id != id);
+    }
+    add(item) {
+        this.collection = this.collection.concat([item]);
+    }
+}
--- test/CatModel.js
+++ test/CatModel.js
@@ -6,19 +6,76 @@ import CatModel from '../src/CatModel';
 describe('Category Model', () => {
     'use strict';
 
-    var fakeCatList = [{
-        name: 'cat1',
-        number: '12'
-    }];
-    var fakeStorage = {
+    const fakeCat = {
+        id: '20',
+        name: 'new cat',
+        number: '0'
+    };
+    const fakeCatList = [
+        {
+            id: '76',
+            name: 'cat1',
+            number: '12'
+        },
+        {
+            id: '98',
+            name: 'cat2',
+            number: '123'
+        }
+    ];
+    const increasedCatList = [
+        {
+            id: '76',
+            name: 'cat1',
+            number: '12'
+        },
+        {
+            id: '98',
+            name: 'cat2',
+            number: '123'
+        },
+        {
+            id: '20',
+            name: 'new cat',
+            number: '0'
+        }
+    ];
+    const decreasedCatList = [
+        {
+            id: '98',
+            name: 'cat2',
+            number: '123'
+        }
+    ];
+    const fakeStorage = {
         getItem: _.constant(JSON.stringify(fakeCatList)),
         setItem: _.noop
     };
 
     it('should get saved categories from storage', () => {
-        var catList = new CatModel(fakeStorage).collection;
+        const catList = new CatModel(fakeStorage).collection;
 
         expect(catList).to.deep.equal(fakeCatList);
+    });
+
+    it('should add a new Category', () => {
+        const catModel = new CatModel(fakeStorage);
+
+        expect(catModel.collection).to.deep.equal(fakeCatList);
+
+        catModel.add(fakeCat);
+
+        expect(catModel.collection).to.deep.equal(increasedCatList);
+    });
+
+    it('should remove a Category', () => {
+        const catModel = new CatModel(fakeStorage);
+
+        expect(catModel.collection).to.deep.equal(fakeCatList);
+
+        catModel.remove('76');
+
+        expect(catModel.collection).to.deep.equal(decreasedCatList);
     });
 });
 
